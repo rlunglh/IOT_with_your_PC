a0e29b11131c9f79f7bc29632eeb3e00ed232b3a
rlunglh <rlunglh@users.noreply.github.com>
2018-06-21 13:36:44 -0400

    Add files via upload

---
+++ relayCycle.ino
@@ -0,0 +1,14 @@
+#deffine RELAY D1
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200); /// use 115200 as the serial baud rate
+  pinMode(RELAY,OUTPUT);
+}
+
+void loop() {
+#define RelayOn 1
+  digitalWrite(RELAY,RelayOn);
+  delay(1000); 
+  digitalWrite(RELAY,!RelayOn);
+  delay(1000);
+}
