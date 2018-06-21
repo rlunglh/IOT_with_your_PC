933222cb020b4dd5de35258cbf73b961b6f1abb7
rlunglh <rlunglh@users.noreply.github.com>
2018-06-21 12:14:31 -0400

    Add files via upload

---
+++ BlinkOnboard.ino
@@ -0,0 +1,17 @@
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200); /// use 115200 as the serial baud rate
+  pinMode(LED_BUILTIN,OUTPUT);
+}
+
+void loop() {
+#define LEDon 0
+  digitalWrite(LED_BUILTIN,LEDon);
+  delay(100); 
+  digitalWrite(LED_BUILTIN,!LEDon);
+  delay(100);
+  digitalWrite(LED_BUILTIN,LEDon);
+  delay(100);
+  digitalWrite(LED_BUILTIN,!LEDon);
+  delay(800);
+}
