49aa0975ebf8df1d7200fef81b9254234d4c3b73
rlunglh <rlunglh@users.noreply.github.com>
2018-06-21 12:32:06 -0400

    Add files via upload

--- BlinkWiredLED.ino
+++ BlinkWiredLED.ino
@@ -1,17 +1,17 @@
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200); /// use 115200 as the serial baud rate
-  pinMode(D3,OUTPUT);
+  pinMode(D7,OUTPUT);
 }
 
 void loop() {
 #define LEDon 1
-  digitalWrite(D3,LEDon);
+  digitalWrite(D7,LEDon);
   delay(100); 
-  digitalWrite(D3,!LEDon);
+  digitalWrite(D7,!LEDon);
   delay(100);
-  digitalWrite(D3,LEDon);
+  digitalWrite(D7,LEDon);
   delay(100);
-  digitalWrite(D3,!LEDon);
+  digitalWrite(D7,!LEDon);
   delay(800);
 }
