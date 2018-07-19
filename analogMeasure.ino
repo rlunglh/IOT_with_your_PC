950f442fc555ec93ae6ef3803a2cd34aa838a514
rlunglh <rlunglh@users.noreply.github.com>
2018-07-19 12:26:31 -0400

    Add files via upload

---
+++ analogMeasure.ino
@@ -0,0 +1,11 @@
+void setup() {
+  Serial.begin(115200);
+  Serial.println();
+}
+ 
+void loop() {
+  int val=analogRead(0);
+  float volts=val*3.3/1024;
+  Serial.printf("Voltage is %0.2f\n",volts);
+  delay(2000);
+}
