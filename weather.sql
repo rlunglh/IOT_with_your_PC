9e16bb1481f5008dd5b2da0737ba8b0c27c02777
rlunglh <rlunglh@users.noreply.github.com>
2018-08-26 14:37:12 -0400

    Add files via upload

--- weather.sql
+++ weather.sql
@@ -1,5 +1,5 @@
-CREATE TABLE `Weather` (
-  `Name` varchar(20) NOT NULL,
+CREATE TABLE `weather` (
+  `Name` varchar(64) NOT NULL,
   `Value` varchar(64) NOT NULL DEFAULT '',
   PRIMARY KEY (`Name`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1
