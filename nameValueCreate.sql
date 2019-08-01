c6e741abaa636a6323ea094e6c4c074474417ba0
Rick Lunglhofer <rlunglh@microimagesys.com>
2019-08-01 18:23:55 -0400

    Add files via upload

--- nameValueCreate.sql
+++ nameValueCreate.sql
@@ -3,3 +3,9 @@ CREATE TABLE `namevalue` (
   `Value` varchar(64) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
   PRIMARY KEY (`Name`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+CREATE TABLE `timedNameValue` (
+  `TimedNameValue_id` datetime NOT NULL,
+  `Name` nvarchar(64) DEFAULT NULL,
+  `Value` nvarchar(64) DEFAULT NULL,
+  PRIMARY KEY (`TimedNameValue_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
